b52dfeccc84cc68507ea777776049958ae2639ef
channddra <43722552+channddra@users.noreply.github.com>
2020-02-26 15:17:46 +0100

    edit

--- app/src/main/java/com/example/multipleactivity/DobActivity.java
+++ app/src/main/java/com/example/multipleactivity/DobActivity.java
@@ -7,12 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.CalendarView;
 import android.widget.DatePicker;
 
 public class DobActivity extends AppCompatActivity {
     String name, address, date;
-    CalendarView simpleCalendarView;
     int y, m, d;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
